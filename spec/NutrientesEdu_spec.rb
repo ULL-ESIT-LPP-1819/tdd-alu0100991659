f5efc370267bb278fcaa74facae9311c21053f74
Eduardo <dimion0096@gmail.com>
2018-11-12 13:43:13 +0000

    Prueba para el nombre definida

--- spec/NutrientesEdu_spec.rb
+++ spec/NutrientesEdu_spec.rb
@@ -2,8 +2,18 @@ RSpec.describe NutrientesEdu do
   it "has a version number" do
     expect(NutrientesEdu::VERSION).not_to be nil
   end
-
+  
+  before :all do
+    @pistacho = Alimento.new("pistacho")
+  end
+  
   it "does something useful" do
     expect(true).to eq(true)
   end
+  
+  context "Elementos básicos del alimento" do
+    it "Tiene nombre" do
+      expect(@pistacho).to have_attributes(:nombre => "pistacho")
+    end
+end
 end
