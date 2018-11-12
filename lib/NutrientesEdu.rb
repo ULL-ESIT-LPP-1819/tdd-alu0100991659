fe5dc6f1acf27949e25d0f490c27b093936bd3f1
Eduardo <dimion0096@gmail.com>
2018-11-12 14:04:04 +0000

    Codificado lo necesario para la prueba de las proteinas

--- lib/NutrientesEdu.rb
+++ lib/NutrientesEdu.rb
@@ -2,14 +2,15 @@ require "NutrientesEdu/version"
 
   class Alimento
     
-    attr_reader :nombre, :grasas, :grsat, :carbs, :azucar
+    attr_reader :nombre, :grasas, :grsat, :carbs, :azucar, :protein
     
-    def initialize(nombre, grasas, grsat, carbs, azucar)
+    def initialize(nombre, grasas, grsat, carbs, azucar, protein)
       @nombre = nombre
       @grasas = grasas
       @grsat = grsat
       @carbs = carbs
       @azucar = azucar
+      @protein = protein
       
     end
   end
