a19f44ebeeffe92458c819039fd42cbb9b4617b2
Eduardo <dimion0096@gmail.com>
2018-11-12 13:55:34 +0000

    Codificado lo necesario para la prueba de las grasas saturadas

--- lib/NutrientesEdu.rb
+++ lib/NutrientesEdu.rb
@@ -2,10 +2,12 @@ require "NutrientesEdu/version"
 
   class Alimento
     
-    attr_reader :nombre, :grasas
+    attr_reader :nombre, :grasas, :grsat
     
-    def initialize(nombre, grasas)
+    def initialize(nombre, grasas, grsat)
       @nombre = nombre
       @grasas = grasas
+      @grsat = grsat
+      
     end
   end
