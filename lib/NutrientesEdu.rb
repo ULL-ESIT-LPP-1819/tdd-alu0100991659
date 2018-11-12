c499aed5fe61b607c9b85d73621dc1dfc6f8533a
Eduardo <dimion0096@gmail.com>
2018-11-12 14:01:59 +0000

    Codificado lo necesario para la prueba de los azucares

--- lib/NutrientesEdu.rb
+++ lib/NutrientesEdu.rb
@@ -2,13 +2,14 @@ require "NutrientesEdu/version"
 
   class Alimento
     
-    attr_reader :nombre, :grasas, :grsat, :carbs
+    attr_reader :nombre, :grasas, :grsat, :carbs, :azucar
     
-    def initialize(nombre, grasas, grsat, carbs)
+    def initialize(nombre, grasas, grsat, carbs, azucar)
       @nombre = nombre
       @grasas = grasas
       @grsat = grsat
       @carbs = carbs
+      @azucar = azucar
       
     end
   end
