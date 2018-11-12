d6d4d427607643100512885b6319f15e4b822467
Eduardo <dimion0096@gmail.com>
2018-11-12 13:58:28 +0000

    Prueba para los hidratos de carbono definida

--- spec/NutrientesEdu_spec.rb
+++ spec/NutrientesEdu_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe NutrientesEdu do
   end
   
   before :all do
-    @pistacho = Alimento.new("pistacho", 20, 5)
+    @pistacho = Alimento.new("pistacho", 20, 5, 30)
   end
   
   it "does something useful" do
@@ -21,5 +21,9 @@ RSpec.describe NutrientesEdu do
     it "Tiene cantidad de grasas saturadas" do
       expect(@pistacho).to have_attributes(:grsat => 5 )
     end
+    it "Cantidad de hidratos de carbono" do
+      expect(@pistacho).to have_attributes(:carbs => 30)
+    end
+  
 end
 end
