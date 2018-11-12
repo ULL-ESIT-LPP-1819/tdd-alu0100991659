7b400adcfaa7049e9f364fec53ffea75b9838bad
Eduardo <dimion0096@gmail.com>
2018-11-12 14:45:59 +0000

    Creando fichero de pruebas

---
+++ lib/Lista.rb
@@ -0,0 +1,25 @@
+require "NutrientesEdu/version"
+
+  class Alimento
+    
+    attr_reader :nombre, :grasas, :grsat, :carbs, :azucar, :protein, :sal
+    
+    def initialize(nombre, grasas, grsat, carbs, azucar, protein, sal)
+      @nombre = nombre
+      @grasas = grasas
+      @grsat = grsat
+      @carbs = carbs
+      @azucar = azucar
+      @protein = protein
+      @sal = sal
+      
+    end
+    
+    def val_ener
+      (grasas*9+carbs*4+protein*4)
+    end
+    
+    def to_s
+      "Nombre: #{@nombre}, Grasas: #{@grasas}, Saturadas: #{@grsat}, Carbohidratos: #{@carbs}, Azucar: #{@azucar}, Proteinas: #{@protein}, Sal: #{@sal}"
+    end
+  end
---
+++ spec/Lista_spec.rb
@@ -0,0 +1,61 @@
+RSpec.describe Lista do
+  it "has a version number" do
+    expect(Lista::VERSION).not_to be nil
+  end
+  
+  before :all do
+    @pistacho = Alimento.new("pistacho", 20, 5, 30, 3, 60, 1)
+  end
+  
+  it "does something useful" do
+    expect(true).to eq(true)
+  end
+  
+  context "Elementos básicos del alimento" do
+    it "Tiene nombre" do
+      expect(@pistacho).to have_attributes(:nombre => "pistacho")
+    end
+    it "Tiene cantidad de grasas" do
+      expect(@pistacho).to have_attributes(:grasas => 20)
+    end
+    it "Tiene cantidad de grasas saturadas" do
+      expect(@pistacho).to have_attributes(:grsat => 5 )
+    end
+    it "Tiene cantidad de hidratos de carbono" do
+      expect(@pistacho).to have_attributes(:carbs => 30)
+    end
+    it "Tiene cantidad de azucares" do
+      expect(@pistacho).to have_attributes(:azucar => 3)
+    end
+    it "Tiene cantidad de proteinas" do
+      expect(@pistacho).to have_attributes(:protein => 60)
+    end
+    it "Tiene cantidad de sal" do
+      expect(@pistacho).to have_attributes(:sal => 1)
+    end
+    it "Tiene metodo para obtener nombre" do
+      expect(@pistacho.nombre).to eq("pistacho")
+    end
+    it "Tiene metodo para obtener cantidad de grasas" do
+      expect(@pistacho.grasas).to eq(20)
+    end
+    it "Tiene metodo para obtener cantidad de grasas saturadas" do
+      expect(@pistacho.grsat).to eq(5)
+    end
+    it "Tiene metodo para obtener cantidad de carbohidratos" do
+      expect(@pistacho.carbs).to eq(30)
+    end
+    it "Tiene metodo para obtener cantidad de azucares" do
+      expect(@pistacho.azucar).to eq(3)
+    end
+    it "Tiene metodo para obtener la cantidad de sal" do
+      expect(@pistacho.sal).to eq(1)
+    end
+    it "Tiene metodo para obtener el valor energetico" do
+      expect(@pistacho.val_ener).to eq(20*9+30*4+60*4)
+    end
+    it "Tiene metodo para obtener la etiqueta formateada" do
+      expect(@pistacho.to_s).to eq("Nombre: pistacho, Grasas: 20, Saturadas: 5, Carbohidratos: 30, Azucar: 3, Proteinas: 60, Sal: 1")
+    end
+end
+end
