92691f350aab5685a49fb9bf4abc6a64330e66d1
Eduardo <dimion0096@gmail.com>
2018-11-12 14:05:28 +0000

    Codificado lo necesario para la prueba de la sal

--- lib/NutrientesEdu.rb
+++ lib/NutrientesEdu.rb
@@ -2,15 +2,16 @@ require "NutrientesEdu/version"
 
   class Alimento
     
-    attr_reader :nombre, :grasas, :grsat, :carbs, :azucar, :protein
+    attr_reader :nombre, :grasas, :grsat, :carbs, :azucar, :protein, :sal
     
-    def initialize(nombre, grasas, grsat, carbs, azucar, protein)
+    def initialize(nombre, grasas, grsat, carbs, azucar, protein, sal)
       @nombre = nombre
       @grasas = grasas
       @grsat = grsat
       @carbs = carbs
       @azucar = azucar
       @protein = protein
+      @sal = sal
       
     end
   end
