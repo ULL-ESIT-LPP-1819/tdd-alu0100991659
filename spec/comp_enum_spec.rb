01ac3c93ba359409588a037a44a6981b192f5822
Eduardo <dimion0096@gmail.com>
2018-12-05 04:12:11 +0000

    Prueba codificada para comprobar el comparable menor para los individuos

--- spec/comp_enum_spec.rb
+++ spec/comp_enum_spec.rb
@@ -24,6 +24,27 @@ RSpec.describe NutrientesEdu do
     it "Prueba para comparable igual" do
       expect(@alimento1 == @alimento2).to eq(false)
     end
+    it "Prueba para comparable between" do
+      alimentoaux=Alimento.new("pera", 6, 1, 10, 6, 1, 1)
+      expect(alimentoaux.between?(@alimento1,@alimento2)).to eq(true)
+    end
+  end
+end
+
+RSpec.describe Individuos do
+  it "has a version number" do
+    expect(Individuos::VERSION).not_to be nil
+  end
+  
+  before :all do
+    @individuo1 = Individuos.new("Marta", 1, 1, 20, 1.60, 50, 0)
+    @individuo2 = Individuos.new("Peter", 1, 1, 150, 1.60, 13, 1)
+  end
+  
+  context "Pruebas de comparación para los Individuos (Por IMC)" do
+    it "Prueba para comparable menor" do
+      expect(@individuo1 < @individuo2).to eq(true)
+    end
   end
 end
       
